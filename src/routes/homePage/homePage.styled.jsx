1e7dbdcfa46945913fe9cb7eefd2c7649622bf71
Berserk29 <oliviertrudeau777@gmail.com>
2023-09-16 14:31:29 -0400

    Fixed small issue on Safari for the container width

--- src/routes/homePage/homePage.styled.jsx
+++ src/routes/homePage/homePage.styled.jsx
@@ -5,15 +5,14 @@ const browser = detect();
 
 export const Container = styled.div`
     /* Disable Scroll-snap for safari */
-    width: 100vw;    
+    width: 100%;    
     height: 100vh;
-    overflow-y: scroll;
-
+    
     /* If lower than 650px will not get code */
     @media ${mediaQuery.styledMinHeight} {
-
+        
         ${() => {
-        if(browser && browser.name !== 'safari') {
+            if(browser && browser.name !== 'safari') {
             return `
             ::-webkit-scrollbar {
                 display: none
